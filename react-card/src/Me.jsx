18e0bafe8c9a7e74642871f74efd11ff597946e1
Manjot Kaur <kaurmanjot2074@outlook.com>
2024-04-03 23:21:58 +0530

    first commit

---
+++ react-card/src/Me.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Me() {
+  return (
+    <div className='details'>
+      <h1>Manjot Kaur</h1>
+      <p>I am diving into Full-Stack Web Development and exploring UI/UX design and AI/ML. Sharing my insights on Twitter (@kaurmanjot20), I aim to contribute to the tech community.</p>
+    </div>
+  )
+}
