c1917f002eb1c6c5360228a28ba895779c30e3cc
Manjot Kaur <kaurmanjot2074@outlook.com>
2024-04-04 18:16:10 +0530

    Add profile picture to Me component

--- react-card/src/Me.jsx
+++ react-card/src/Me.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
-
+import mkpic from './assets/mkpic.png'
 export default function Me() {
   return (
     <div className='details'>
+      <img src={mkpic} alt="my pic" className='mkpic'/>
       <h1>Manjot Kaur</h1>
       <p>I am diving into Full-Stack Web Development and exploring UI/UX design and AI/ML. Sharing my insights on Twitter (@kaurmanjot20), I aim to contribute to the tech community.</p>
     </div>
